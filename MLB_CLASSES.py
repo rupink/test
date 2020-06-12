025df3eb147926293c535f111e23ac33ad3d4047
rupink <66811124+rupink@users.noreply.github.com>
2020-06-11 22:33:01 -0400

    Add files via upload

---
+++ MLB_CLASSES.py
@@ -0,0 +1,12 @@
+class Rupin:
+    def __init__(self, name):
+        self.name = name
+
+def main():
+    pooper = Rupin('hey')
+    print(pooper.name)
+
+if __name__ == '__main__':
+    main()
+    
+    
